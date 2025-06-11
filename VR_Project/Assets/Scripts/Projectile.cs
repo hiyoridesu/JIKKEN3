4b76afce090144b0e869412e8a17c447275b7361
ZUO HONGYI <160451772+hiyoridesu@users.noreply.github.com>
2025-06-11 14:56:59 +0900

    Add files via upload

---
+++ VR_Project/Assets/Scripts/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    [Header("Shooting Parameter")]
+    [Tooltip("V0(m/s)")]
+    public float initialSpeed = 20f;
+
+    [Tooltip("Initial Direction, Only for Initial Speed. When in 'World' Space, Please make sure it is Unit Vector.")]
+    public Vector3 launchDirection = Vector3.forward;
+
+    [Tooltip("Auto Shoot？true:false")]
+    public bool autoLaunch = true;
+
+    private Rigidbody _rb;
+    private bool _launched = false;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        if (_rb == null)
+        {
+            Debug.LogError("Projectile: Less of Rigidbody Components");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VR_Project/Assets/Scripts/TipCollisionDetector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipCollisionDetector : MonoBehaviour
+{
+    [Header("可在 Inspector 中修改的伤害值或ID")]
+    public int damage = 1;
+    public string hitSignal = "OnHitByProjectile";
+
+    private bool hasHit = false;
+
+}
